3ad2f27fb76e69144df90e025af7ffd17d78c0e0
Tarsis Lima <br.tmvdl@gmail.com>
2025-10-21 11:55:07 -0300

    Update main Python file

--- src/server/app.py
+++ src/server/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static', static_url_path='/static') 
 
 @app.route('/')
 def home():
@@ -9,17 +9,15 @@ def home():
 
 @app.route('/chat/<chatid>')
 def show_chat(chatid):
-    # show the chat with id
     return render_template('chat.html', chatid=chatid)
 
-# socketio
-
 socketio = SocketIO()
+
 socketio.init_app(app)
 
 @socketio.on('message')
 def handle_message(data):
-    print('received message: ' + data)
+    print('received message: ' + str(data))
 
 @socketio.on('json')
 def handle_json(json):
