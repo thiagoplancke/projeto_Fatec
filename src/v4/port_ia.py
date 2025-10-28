d795c1301a7e51961f7873c4a3efe8da7a78929d
Tarsis Lima <br.tmvdl@gmail.com>
2025-10-28 12:37:52 -0300

    Update v4 files

--- src/v4/port_ia.py
+++ src/v4/port_ia.py
@@ -1,21 +1,19 @@
 from google import genai
 from google.genai import types
+import os
 
-client = genai.Client(api_key="<GEMINI_API_KEY>")
+model="gemini-2.5-flash"
+api_key = os.getenv("GEMINI_API_KEY")
 
-instrucoes = "Você é o Assistente Virtual da Whirlpool."
+client = genai.Client(api_key=api_key)
 
-config = types.GenerateContentConfig(
-  system_instruction=instrucoes
-)
+system_instruction = """
+Você é o Assistente Virtual da Whirlpool. 
+Você pode responder, em texto curtos, sobre produtos Whirlpool.
+"""
 
-# aqui eu só usei para testar mesmo.
-# port_ia.py
-def responder_ia(texto):
-    response = client.models.generate_content(
-        model="gemini-2.5-flash",
-        contents=texto,
-        config=config
-    )
+config = types.GenerateContentConfig(system_instruction=system_instruction)
 
+def responder_ia(contents):
+    response = client.models.generate_content(model=model,contents=contents,config=config)
     return response.text
